ad90adba7b15a8d54727a35af0d8ef0cf8a7c517
viniciusfleury <54606415+viniciusfleury@users.noreply.github.com>
2022-07-04 14:22:25 -0300

    Add files via upload

---
+++ ClientEx7.py
@@ -0,0 +1,26 @@
+import socket
+
+HOST = 'localhost'
+PORT = 15000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST,PORT))
+
+Idade = input()
+Tempo = input()
+
+msg = str(Idade + '|' + Tempo)
+
+s.sendall(str.encode(msg))
+
+data = s.recv(1024)
+
+data = data.decode()
+
+Idade, Tempo = data.split(' ')
+
+
+if data == 'Aposentar'
+    print('Funcionario pode se aposentar')
+else
+    print('FUncionario não pode se aposentar')
