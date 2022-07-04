4dafc660807ad3e22d91a08ef90756bfaecc6cc0
viniciusfleury <54606415+viniciusfleury@users.noreply.github.com>
2022-07-04 13:42:48 -0300

    Add files via upload

---
+++ servidorEx3.py
@@ -0,0 +1,45 @@
+import socket
+
+HOST = 'localhost'
+PORT = 15000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(HOST,PORT))
+
+s.listen()
+
+print('Esperando conexão')
+
+conn, ender = s.accept()
+
+pint('Conectado em', ender)
+
+while True
+    data = conn.recv(1024)
+
+    if not data:
+        print('Conexão fechada')
+        conn.close()
+        break
+
+    data = data.decode()
+
+    N1, N2, N3 = data.split(' ')
+
+    N1 = float(N1)
+    N2 = float(N2)
+    N3 = float(N3)
+
+    m = ((N1 + N2)/2)
+
+    if m >= 7,0:
+        data = 'Aprovado'
+    if m > 3,0 and m < 7,0:
+        if ((m + N3)/2) >= 5,0 :
+            data = 'Aprovado'
+    else
+        data = 'Reprovado'
+
+    data = str.encode(str(N1) + ' ' + str(N2) + ' ' + str(N3))
+
+    conn.sendall(data)
