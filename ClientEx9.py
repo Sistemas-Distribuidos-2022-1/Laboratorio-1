d9f6522e5e7161206e7ba933aa236764e7d32789
viniciusfleury <54606415+viniciusfleury@users.noreply.github.com>
2022-07-04 14:47:06 -0300

    Add files via upload

---
+++ ClientEx9.py
@@ -0,0 +1,23 @@
+import socket
+
+HOST = 'localhost'
+PORT = 15000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST,PORT))
+
+vcarta = input()
+naipe = input()
+
+msg = str(vcarta + '|' + naipe)
+
+s.sendall(str.encode(msg))
+
+data = s.recv(1024)
+
+data = data.decode()
+
+vcarta, naipe = data.split(' ')
+
+
+print('Carta: ', data)
