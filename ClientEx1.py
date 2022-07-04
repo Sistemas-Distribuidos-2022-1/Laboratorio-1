59945c7dc7e6128900bbbed5ce9bd87d2a8f23b1
viniciusfleury <54606415+viniciusfleury@users.noreply.github.com>
2022-07-04 13:14:46 -0300

    Add files via upload

---
+++ ClientEx1.py
@@ -0,0 +1,24 @@
+import socket
+
+HOST = 'localhost'
+PORT = 15000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST,PORT))
+
+nome = input()
+cargo = input()
+salario = input()
+
+msg = str(nome + '|' + cargo + '|' + salario)
+
+s.sendall(str.encode(msg))
+
+data = s.recv(1024)
+
+data = data.decode()
+nome, cargo, salario = data.split(' ')
+
+
+print('Cargo: ', cargo)
+print('Salario com reajuste: ', salario)
