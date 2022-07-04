e3646a4917f1e523d2ef660a72d3d51d646123e1
viniciusfleury <54606415+viniciusfleury@users.noreply.github.com>
2022-07-04 13:48:35 -0300

    Add files via upload

---
+++ ClientEx4.py
@@ -0,0 +1,21 @@
+import socket
+
+HOST = 'localhost'
+PORT = 15000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST,PORT))
+
+Altura = input()
+Sexo = input()
+
+msg = str(Altura + '|' + Sexo)
+
+s.sendall(str.encode(msg))
+
+data = s.recv(1024)
+
+data = data.decode()
+Altura, Sexo = data.split(' ')
+
+print('Peso: ' data)
