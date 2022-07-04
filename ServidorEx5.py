0bf050f0348797b4250387107249cfd8aa9f1e8d
viniciusfleury <54606415+viniciusfleury@users.noreply.github.com>
2022-07-04 13:55:54 -0300

    Add files via upload

---
+++ ServidorEx5.py
@@ -0,0 +1,45 @@
+import socket
+
+HOST = 'localhost'
+PORT = 15000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(HOST,PORT))
+
+s.listen()
+
+print('Esperando conexão')
+
+conn, ender = s.accept()
+
+pint('Conectado em', ender)
+
+while True
+    data = conn.recv(1024)
+
+    if not data:
+        print('Conexão fechada')
+        conn.close()
+        break
+
+    data = data.decode()
+
+    Idade = int(Idade)
+
+    if Idade >=5 and Idade <= 7:
+        data = 'infantil A'
+    else:
+        if Idade >=8 and Idade <= 10:
+            data  = 'infantil B'
+        else:
+            if Idade >= 11 and Idade <= 13:
+                data = ' Juvenil A'
+            else:
+                if Idade >= 14 and Idade <= 17:
+                    data = 'Juvenil B'
+                else:
+                    data = 'adulto'
+
+    data = str.encode(str(Idade))
+
+    conn.sendall(data)
