fd1f84b9984b59db45d7b76d0243a43ded73c0b2
viniciusfleury <54606415+viniciusfleury@users.noreply.github.com>
2022-07-04 13:32:18 -0300

    Add files via upload

---
+++ ClientEx2.py
@@ -0,0 +1,28 @@
+import socket
+
+HOST = 'localhost'
+PORT = 15000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST,PORT))
+
+nome = input()
+sexo = input()
+idade = input()
+
+msg = str(nome + '|' + sexo + '|' + idade)
+
+s.sendall(str.encode(msg))
+
+data = s.recv(1024)
+
+data = data.decode()
+nome, sexo, idade = data.split(' ')
+
+
+print('nome: ', nome)
+print('sexo: ', sexo)
+if data == 'maioridade':
+    print('Atingiu maioridade')
+else:
+    print('Não atingiu maioridade')
